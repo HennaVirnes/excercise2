f3355f0a122765939cde71dc6173e28139347a99
Kalle Soukka <t9soka00@students.oamk.fi>
2019-09-27 13:08:50 +0300

    Added lyrics

--- hakunamatata.js
+++ hakunamatata.js
@@ -1,1 +1,4 @@
 process.stdout.write ("\n Hakunamatata is the best song ever!!!!\n"); 
+
+process.stdout.write ("\n Hakuna matata. What a wonderful word! "
+                        + "\n hakuna matata. Ain't no self asdf! \n\n") ;
