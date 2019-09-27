841915f134b2d1fa7626e6799aad33b43b1cdcc8
Henna Virnes <t9vihe00@students.oamk.fi>
2019-09-27 13:26:10 +0300

    copied this game from different location

--- eleven.js
+++ eleven.js
@@ -1,5 +1,17 @@
+process.stdout.write( "\n This program is a computer game. Please, "
+                    + "\n type in a number : " ) ;
 
+process.stdin.on( "data", function( input_from_user )
+{
 
-var my_array = [1, 2, 3, 4, 5]
+   var given_number = Number( input_from_user ) ;
 
-if ( my_array =askfjk)
+   var winning_number = given_number + 1 ;
+
+   process.stdout.write( "\n You typed in " + given_number + "."
+                      +  "\n My number is " + winning_number  + "."
+                      +  "\n Sorry, you lost. I won. The game is over.\n\n" ) ;
+
+   process.exit() ;
+   
+   } ) ;
